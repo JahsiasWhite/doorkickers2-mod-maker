ddb8f183728fbf22af73b173e923e4b9159cc0f8
Jah <jahsias.white@gmail.com>
2025-03-05 21:34:00 -0600

    added attack type modifier

---
+++ src/constants/AttackTypeModifier.js
@@ -0,0 +1,9 @@
+const AttackTypeModifier = [
+  { label: 'minAimTime2', value: 0 },
+  { label: 'maxAimTime2', value: 0 },
+  { label: 'resetTime', value: 0 },
+  { label: 'accuracyAdd', value: 0 },
+  { label: 'critChanceAdd', value: 0 },
+];
+
+export default AttackTypeModifier;
