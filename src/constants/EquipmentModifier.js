34ec0a8da5d79da47a711163dfe7008892462f59
Jah <jahsias.white@gmail.com>
2025-03-08 00:31:19 -0600

    added EquipmentModifier

---
+++ src/constants/EquipmentModifier.js
@@ -0,0 +1,10 @@
+const EquipmentModifier = [
+  { label: 'reloadTime', value: 0 },
+  { label: 'reloadEmptyTime', value: 0 },
+  { label: 'readyTime', value: 0 },
+  { label: 'guardTime', value: 0 },
+  { label: 'changeInTime', value: 0 },
+  { label: 'changeOutTimes', value: 0 },
+];
+
+export default EquipmentModifier;
